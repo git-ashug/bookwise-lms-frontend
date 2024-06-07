b19e830437b614e65725719c56e97e900e63056f
Ashutosh <ashutosh211gupta@gmail.com>
2024-06-07 22:19:32 +0530

    Added Bootstrap CSS and JS and static files

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Welcome to BookWise App!</div>;
+  return (
+    <div className="App">
+      <h1>Welcome to BookWise App!</h1>
+    </div>
+  );
 }
 
 export default App;
