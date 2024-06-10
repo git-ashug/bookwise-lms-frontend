9ad04976e7029b4b5e79a074a9b390957f45f556
Ashutosh <ashutosh211gupta@gmail.com>
2024-06-10 21:10:20 +0530

    Added carousel component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 import { ExploreTopBooks } from "./layouts/NavbarAndFooter/HomePage/ExploreTopBooks";
+import { Carousel } from "./layouts/NavbarAndFooter/HomePage/Carousel";
 
 function App() {
   return (
     <div>
       <Navbar />
       <ExploreTopBooks />
+      <Carousel />
     </div>
   );
 }
