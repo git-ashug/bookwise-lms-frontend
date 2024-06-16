4a04277057664f20f835bf17548a6447708eddf4
Ashutosh <ashutosh211gupta@gmail.com>
2024-06-16 13:46:45 +0530

    Restructuring the app

--- src/layouts/HomePage/components/ReturnBook.tsx
+++ src/layouts/HomePage/components/ReturnBook.tsx
@@ -5,7 +5,7 @@ export const ReturnBook = () => {
     <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3">
       <div className="text-center">
         <img
-          src={require("./../../Images/BooksImages/new-book-1.png")}
+          src={require("./../../../Images/BooksImages/new-book-1.png")}
           alt="book"
           height={"233"}
           width={"151"}
