7105dbf99ffe37187880e26d054188f89a0c2c7c
Ashutosh <ashutosh211gupta@gmail.com>
2024-09-11 22:46:08 +0530

    Resolved bugs related to setting page number, display of dropdown when selecting through searchbar

--- src/layouts/SearchBooksPage/SearchBooksPage.tsx
+++ src/layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -31,7 +31,11 @@ export const SearchBooksPage = () => {
       if (searchUrl === "") {
         url = `${baseUrl}?page=${currentPage - 1}&size=${booksPerPage}`;
       } else {
-        url = baseUrl + searchUrl;
+        let searchWithPage = searchUrl.replace(
+          "<pageNumber>",
+          `${currentPage - 1}`
+        );
+        url = baseUrl + searchWithPage;
       }
 
       const response = await fetch(url);
@@ -81,16 +85,19 @@ export const SearchBooksPage = () => {
   }
 
   const searchHandleChange = () => {
+    setCurrentPage(1);
     if (search === "") {
       setSearchUrl("");
     } else {
       setSearchUrl(
-        `/search/findByTitleContaining?title=${search}&page=0&size=${booksPerPage}`
+        `/search/findByTitleContaining?title=${search}&page=<pageNumber>&size=${booksPerPage}`
       );
     }
+    setCategorySelection("Book Category");
   };
 
   const categoryField = (value: string) => {
+    setCurrentPage(1);
     if (
       value.toLowerCase() === "fe" ||
       value.toLowerCase() === "be" ||
@@ -99,11 +106,11 @@ export const SearchBooksPage = () => {
     ) {
       setCategorySelection(value);
       setSearchUrl(
-        `/search/findByCategory?category=${value}&page=0&size=${booksPerPage}`
+        `/search/findByCategory?category=${value}&page=<pageNumber>&size=${booksPerPage}`
       );
     } else {
       setCategorySelection("All");
-      setSearchUrl(`?page=0&size=${booksPerPage}`);
+      setSearchUrl(`?page=<pageNumber>&size=${booksPerPage}`);
     }
   };
 
