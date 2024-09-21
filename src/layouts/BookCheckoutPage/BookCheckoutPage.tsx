73bf02331bb0cb5d7a4a5d822ae7d48ee0007c84
Ashutosh <ashutosh211gupta@gmail.com>
2024-09-21 22:11:46 +0530

    Added CheckoutAndReviewPage component

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import BookModel from "../../models/BookModel";
 import { SpinnerLoading } from "../Utils/SpinnerLoading";
 import { StarsReview } from "../Utils/StarsReview";
+import { CheckoutAndReviewPage } from "./CheckoutAndReviewPage";
 
 export const BookCheckoutPage = () => {
   const [book, setBook] = useState<BookModel>();
@@ -78,6 +79,7 @@ export const BookCheckoutPage = () => {
               <StarsReview rating={4.5} size={32} />
             </div>
           </div>
+          <CheckoutAndReviewPage book={book} mobile={false} />
         </div>
         <hr />
       </div>
@@ -104,6 +106,7 @@ export const BookCheckoutPage = () => {
             <StarsReview rating={4.5} size={32} />
           </div>
         </div>
+        <CheckoutAndReviewPage book={book} mobile={true} />
         <hr />
       </div>
     </div>
